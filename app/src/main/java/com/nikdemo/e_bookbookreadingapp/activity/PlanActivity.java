231435040d5df8dc7889f04868f944f434b547d5
ravipatel <102030@@>
2019-06-21 18:52:27 +0530

    change in login design

--- app/src/main/java/com/nikdemo/e_bookbookreadingapp/activity/PlanActivity.java
+++ app/src/main/java/com/nikdemo/e_bookbookreadingapp/activity/PlanActivity.java
@@ -3,6 +3,7 @@ package com.nikdemo.e_bookbookreadingapp.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nikdemo.e_bookbookreadingapp.R;
 import com.nikdemo.e_bookbookreadingapp.utils.BaseActivity;
 
 public class PlanActivity extends BaseActivity {
